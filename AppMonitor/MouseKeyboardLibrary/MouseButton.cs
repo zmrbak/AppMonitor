fd3039e5a97d1323c240f79ed92665a7e447cd47
庆明 赵 <20995658@qq.com>
2021-05-26 12:30:47 +0800

    创建程序添加项目文件。

---
+++ AppMonitor/MouseKeyboardLibrary/MouseButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppMonitor.MouseKeyboardLibrary
+{
+    /// <summary>
+    /// Mouse buttons that can be pressed
+    /// </summary>
+    public enum MouseButton
+    {
+        Left = 0x2,
+        Right = 0x8,
+        Middle = 0x20
+    }
+}
